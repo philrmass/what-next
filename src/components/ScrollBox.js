ce36f92f50ac06a6a446fdb2767ee0bc31467356
Phil Mass <philrmass@gmail.com>
2019-10-29 16:54:31 -0700

    add functions for scroll

--- src/components/ScrollBox.js
+++ src/components/ScrollBox.js
@@ -7,15 +7,15 @@ function ScrollBox({
 }) {
   const [top, setTop] = useState(0);
   //const [start, setStart] = useState(null);
-  const [last, setLast] = useState(null);
+  const [lastY, setLastY] = useState(null);
 
   function handleStart(e) {
     //const yOffsetMin = box.clientHeight - box.scrollHeight;
-    console.log('d', Math.round(e.touches[0].clientX), Math.round(e.touches[0].clientY));
-    console.log('DE', e.currentTarget.clientHeight, e.currentTarget.scrollHeight);
-    const pos = [e.touches[0].clientX, e.touches[0].clientY];
+    //console.log('d', Math.round(e.touches[0].clientX), Math.round(e.touches[0].clientY));
+    //console.log('DE', e.currentTarget.clientHeight, e.currentTarget.scrollHeight);
+    //const pos = [e.touches[0].clientX, e.touches[0].clientY];
     //setStart(pos);
-    setLast(pos);
+    setLastY(getY(e));
   }
 
   function handleEnd() {
@@ -24,7 +24,10 @@ function ScrollBox({
   }
 
   function handleMove(e) {
-    const pos = [e.touches[0].clientX, e.touches[0].clientY];
+    //const pos = [e.touches[0].clientX, e.touches[0].clientY];
+    setOffset(e);
+
+    /*
     const diffY = pos[1] - last[1];
     setTop((top) => {
       const val = top + diffY;
@@ -33,12 +36,48 @@ function ScrollBox({
       }
       return val;
     });
-    console.log('m', Math.round(top));
-    const p = e.currentTarget.parentNode;
-    console.log('e', e.currentTarget.offsetHeight, e.currentTarget.clientHeight, e.currentTarget.scrollHeight);
-    console.log('p', p.offsetHeight, p.clientHeight, p.scrollHeight);
+    */
+    //console.log('m', Math.round(top));
+    //const p = e.currentTarget.parentNode;
+    //console.log('e', e.currentTarget.offsetHeight, e.currentTarget.clientHeight, e.currentTarget.scrollHeight);
+    //console.log('p', p.offsetHeight, p.clientHeight, p.scrollHeight);
     //console.log(`m (${Math.round(pos[0] - start[0])}, ${Math.round(pos[1] - start[1])}) <${Math.round(pos[0] - last[0])}, ${Math.round(pos[1] - last[1])}>`);
-    setLast(pos);
+    setLastY(getY(e));
+  }
+
+  function getY(event) {
+    return event.touches[0].clientY;
+  }
+
+  function getDeltaY(event) {
+    const y = getY(event);
+    if (lastY) {
+      return y - lastY;
+    }
+    return 0;
+  }
+
+  function getYMin(event) {
+    const parent = event.currentTarget.parentNode;
+    return parent.clientHeight - parent.scrollHeight;
+  }
+
+  function setOffset(event) {
+    const deltaY = getDeltaY(event);
+    const yMin = getYMin(event);
+    const yMax = 0;
+    //??? remove
+    console.log('OFF delta', deltaY.toFixed(1), 'min', yMin, 'top', top.toFixed(1));
+
+    setTop((top) => {
+      const value = top + deltaY;
+      if (value > yMax) {
+        return yMax;
+      } else if (value < yMin) {
+        return yMin;
+      }
+      return value;
+    });
   }
 
   return (
